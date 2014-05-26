9bd18f94ddc1e93b7de19570451a0ad7dcf587d5
jeffclarkk <jeffrey.clark@eagles.ewu.edu>
2014-05-26 15:40:20 -0700

    uh

---
+++ Question.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaMaze
+{
+    class Question
+    {
+        private string category = ""; //I'd much rather we did this as an int
+        private int type; //mulitple choice 1, t/f 2
+        private string question = "";
+        private string answer = "";
+        private string dummy1 = "", dummy2 = "", dummy3 = ""; //Wrong answers. For t/f, only need dummy1
+    }
+}
