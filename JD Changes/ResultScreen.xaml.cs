5b73f4856dcf6b68afeebc27092ad030fac12c24
ewu-lopezg <glopez22@eagles.ewu.edu>
2014-06-03 00:23:30 -0700

    help/result/credit screen

---
+++ JD Changes/ResultScreen.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TriviaMaze
+{
+    /// <summary>
+    /// Interaction logic for ResultScreen.xaml
+    /// </summary>
+    public partial class ResultScreen : Window
+    {
+        public ResultScreen()
+        {
+            InitializeComponent();
+            ImageSource isource = new BitmapImage((new Uri(@"images/titleScreen.png", UriKind.Relative)));
+            this.ResultColor.Background = new ImageBrush(isource);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
